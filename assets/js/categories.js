a8e5ceef2b6079012fdad37af57a5160ea6248ab
Демин Максим <lazerbake@gmail.com>
2024-09-19 11:17:43 +0500

    some problem with the icon display

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -18,7 +18,7 @@ export function initializeCategories() {
     function hoverEffect() {
       button.style.border = "2px solid var(--color-lime)"; // Изменяем границу кнопки.
       button.style.backgroundColor = "var(--color-lime)"; // Меняем цвет фона кнопки.
-      button.style.backgroundImage = 'url("../assets/icons/arrow-lavender.svg")'; // Меняем иконку на стрелку другого цвета.
+      button.style.backgroundImage = 'url("./assets/icons/arrow-lavender.svg)'; // Меняем иконку на стрелку другого цвета.
       button.style.transition =
         "border 0.5s ease, background-color 0.5s ease, background-image 0.5s ease"; // Добавляем плавный переход для всех изменений.
     }
@@ -27,7 +27,7 @@ export function initializeCategories() {
     function resetEffect() {
       button.style.border = "2px solid var(--color-lime)"; // Сбрасываем границу.
       button.style.backgroundColor = "transparent"; // Возвращаем прозрачный фон.
-      button.style.backgroundImage = 'url("../assets/icons/arrow-lime.svg")'; // Возвращаем исходную иконку.
+      button.style.backgroundImage = 'url("./assets/icons/arrow-lime.svg")'; // Возвращаем исходную иконку.
     }
 
     // Применяем эффекты при наведении и уходе курсора с элемента категории.
